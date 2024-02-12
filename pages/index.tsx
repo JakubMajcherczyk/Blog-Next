9cdf2e6a3ba10cc7cd603b0f58f70b70d0c123b9
JakubMajcherczyk <123877028+JakubMajcherczyk@users.noreply.github.com>
2024-02-12 19:21:30 +0100

    Stylowanie About i Index

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,11 @@ import posts from "../public/posts.json";
 
 export default function Home() {
   return (
-    <div className="flex flex-wrap justify-center m-6">
+    <div className="flex flex-col items-center m-6">
       <h2 className="text-5xl font-bold tracking-tight text-gray-900 dark:text-white">
         YOUR ARTICLES
       </h2>
-      <div className="flex flex-wrap justify-around items-center m-4 gap-6">
+      <div className="flex flex-wrap justify-center content-start m-4 gap-6 max-w-6xl">
         {posts.map((post) => (
           <PostCard
             key={post.id}
